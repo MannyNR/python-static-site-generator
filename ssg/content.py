e18b28e2cc67fa513a04e4bdde9435f1b5c3dc3a
Emmanuel Nunez <mannynunezr@gmail.com>
2022-10-21 09:59:03 -0400

    Capital L in Loader

--- ssg/content.py
+++ ssg/content.py
@@ -9,7 +9,7 @@ class Content(Mapping):
   @classmethod
   def load(cls, string):
     _, fm, content = cls.__regex.split(string, 2)
-    metadata = load(fm, loader= FullLoader)
+    metadata = load(fm, Loader=FullLoader)
     return cls(metadata, content)
     
   def __init__(self, metadata, content):
